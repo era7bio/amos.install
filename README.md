bf97c6864969a292160290e9d7ba618088655494
Eduardo Pareja-Tobes <eduardo@pareja-tobes.name>
2012-11-19 16:32:49 +0100

    first cut

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 amos.install
 ============
 
-install AMOS
+this will install AMOS. You will need to install mummer first if you want to use certain AMOS stuff, like minimus. For this, [era7bio/mummer.install](https://github.comera7bio/mummer.install)
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -x -e
+
+# AMOS needs mummer for some of its utils
+# use era7bio/mummer.install if needed
+
+cd /opt
+wget http://sourceforge.net/projects/amos/files/amos/3.1.0/amos-3.1.0.tar.gz/download
+tar -xvzf amos-3.1.0.tar.gz
+rm amos-3.1.0.tar.gz
+cd amos-3.1.0
+
+# deps
+yum -y install make gcc zlib zlib-devel zlib-static boost boost-devel \
+  libXmu libXmu-devel libXi libXi-devel expat expat-devel \
+  perl perl-ExtUtils-* \
+
+# install cpanm
+curl -L http://cpanmin.us | perl - App::cpanminus
+# add perl mods
+cpanm Statistics::Descriptive DBI
+
+# configure AMOS
+./configure --prefix=/opt/amos-3.1.0
+make
+make install
+# create links for binaries
+ln -s /opt/amos-3.1.0/bin/* /usr/bin
